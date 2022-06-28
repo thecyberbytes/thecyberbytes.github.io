0e691f2a20228ade619ead28f9ef8b8acbda3d24
thecyberbytes <108113656+thecyberbytes@users.noreply.github.com>
2022-06-28 14:51:32 +0530

    Create gen_card_col_1.js

---
+++ cyber_web/js/gen_card_col_1.js
@@ -0,0 +1,26 @@
+document.write(' \
+<div class="container-fluid border border-dark border-0 align-items-center justify-content-between"> \
+<div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.tripwire.com/state-of-security/security-data-protection/protection-payments-ransomware-businesses/" target="_blank" style="text-decoration:none"> \
+<img src="https://3b6xlt3iddqmuq5vy2w0s5d3-wpengine.netdna-ssl.com/state-of-security/wp-content/uploads/sites/3/Are-Protection-Payments-the-Future-of-Ransomware-How-Businesses-Can-Protect-Themselves-200x113.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Hacking</span> <span class="mx-1">&bullet;</span> <span>June 27, 2022</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">Are Protection Payments the Future of Ransomware? How Businesses Can Protect Themselves</h2> \
+</div> \
+</a> \
+</div> \
+</div> \
+<div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://thehackernews.com/2022/06/cybersecurity-experts-warn-of-emerging.html" target="_blank" style="text-decoration:none"> \
+<img src="https://thehackernews.com/new-images/img/b/R29vZ2xl/AVvXsEjCYB-8ZAsSijLSXQ_z4mmq8d3nOdEZPULCRp23pGdO1D3xx-7QtBjDk-wRK1ehA-9IQkQjQ9auAnlQsGw1yU9c22oqjhiYtqUUFW5WgMSDyqQIAnwAp3S_NYGi0bzMs-GIabEn7chvsUQkCXugHP2zHnX8GAv8RfwkqluD_LQDDfqkgU8X65zPshOp/s728-e100/ransomware.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>June 27, 2022</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">Cybersecurity Experts Warn of Emerging Threat of "Black Basta" Ransomware</h2> \
+</div> \
+</a> \
+</div> \
+</div> \
+</div> \
+');
