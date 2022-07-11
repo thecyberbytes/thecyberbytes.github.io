9a9bcfe000f165f58106621cff23aae51599e196
GitHub Action <mailcyberbytes@gmail.com>
2022-07-11 12:13:29 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_20.js
+++ cyber_web/js/gen_card_col_20.js
@@ -1,5 +1,14 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://thehackernews.com/2022/07/microsoft-quietly-rolls-back-plan-to.html" target="_blank" style="text-decoration:none"> \
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj1d84epDv4jgHnm-m03FayWvFIKm2Ed6cgfOfjJoaza0WC0Pn8mX-HPBjJyej-DSFK5XKbQ8rViQut9fZfVTdjTjp8OgiLhYur3XJIxGscvdjro-pYY3GxVecFTCp3suYxsMSUZM3UluwuTciSC8dKh4tY7UssVL0HCXk2szm-BdMSS4_hWABKvWW_/s260-e100/ms.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>July 08, 2022</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Microsoft Temporarily Rolls Back Plan to Block Office VBA Macros by Default</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://thehackernews.com/2022/07/why-developers-hate-changing-language.html" target="_blank" style="text-decoration:none"> \
 <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg2T_LeUBbsKjXN48gXP4ySosNQDmjrSqqmBT4vqw1e1aHEfDtl0gYMk5uni0txHO3LWQbZBbCOiq8WZTvg_UHb-biK0U5LM98Pgy8NvHWDnkFdwpokpDnMPs-avE9wP9O_Hz8boQyYCmWHQZ2OJzo-fTWET2NVcYGemrj7ztx4g7rHedoZscgrZ2F_/s260-e100/code.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
@@ -14,15 +23,6 @@ document.write(' \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Vulnerability</span> <span class="mx-1">&bullet;</span> <span>July 08, 2022</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Experts Uncover 350 Browser Extension Variants Used in ABCsoup Adware Campaign</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://thehackernews.com/2022/07/trickbot-malware-shifted-its-focus-on.html" target="_blank" style="text-decoration:none"> \
-<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgnRIShm8RNuRNjD42XnhHAzm8eneXnyWirL3jJpP3-MUDuGZ42jXe-2jldE6FteEIzRFk7yGJANXGGNNroyyQM5JMbhb024h9SURqlb_NGkxpfoOolPb6RTfIvt_jpatUyNr_7ouKZhcgcBb1O8i-7WihnZMqqF1mEQothRRubI5WBr-RN7XR4A4Cj/s260-e100/hacking.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Ransomware</span> <span class="mx-1">&bullet;</span> <span>July 07, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">TrickBot Gang Shifted its Focus on \"Systematically\" Targeting Ukraine</h5> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_31.js
+++ cyber_web/js/gen_card_col_31.js
@@ -4,7 +4,9 @@ document.write(' \
 <img src="https://media.cybernews.com/images/thumbnail/2022/07/shutterstock_2081740117.png" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Data Breach</span> <span class="mx-1">&bullet;</span> <span>08 July 2022 </span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Meta sues data scrapers with China and Turkey ties</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">
+Meta sues data scrapers with China and Turkey ties
+</h5> \
 </div> \
 </a> \
 </div> \
@@ -13,7 +15,9 @@ document.write(' \
 <img src="https://media.cybernews.com/images/thumbnail/2022/07/metasuesscrapers.png" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>08 July 2022 </span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">TikTok sued for Blackout Challenge after child deaths</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">
+TikTok sued for Blackout Challenge after child deaths
+</h5> \
 </div> \
 </a> \
 </div> \
@@ -22,7 +26,9 @@ document.write(' \
 <img src="https://media.cybernews.com/images/thumbnail/2022/07/TikTokEvil.png" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>08 July 2022 </span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">China ramps up its cyber espionage efforts against Russia</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">
+China ramps up its cyber espionage efforts against Russia
+</h5> \
 </div> \
 </a> \
 </div> \
