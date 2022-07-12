f254df370f96a1478272a1d55e05620c606538bd
GitHub Action <mailcyberbytes@gmail.com>
2022-07-12 05:43:49 +0000

    Commiting Category data

--- cyber_web/js/gen_card_cat_Crypto Currency.js
+++ cyber_web/js/gen_card_cat_Crypto Currency.js
@@ -1,11 +1,11 @@
 document.write(' \
 <div class="d-md-flex post-entry-2 small-img"> \
-<a href="https://thehackernews.com/2022/07/cloud-based-cryptocurrency-miners.html" class="me-4 thumbnail"> \
+<a href="https://thehackernews.com/2022/07/cloud-based-cryptocurrency-miners.html" class="me-4 thumbnail" target="_blank"> \
 <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhsScxx5lrEQfbCWFs0LL3YwLbAckiMuNSNVSj4Omh9y7t6hBlaO_A6W3i3-h8DTXr3qLYmgGe6_0Rtcf1r1nr8GhXQymONb3lJx43bcg2JJevnRerCZ0F5hkfiVBkcgDnHVzxOJNTWdsD2PRAAaB1xWgrjGOgK4Hv-fXCtLirQqu2d2suiXccZUxxI/s260-e100/ss.jpg" alt="" class="img-fluid"> \
 </a> \
 <div class="card-body p-0 mx-0" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <h3 class="card-link text-dark font-weight-bold"> \
-<a href="https://thehackernews.com/2022/07/cloud-based-cryptocurrency-miners.html"> Cloud-based Cryptocurrency Miners Targeting GitHub Actions and Azure VMs </a> \
+<a href="https://thehackernews.com/2022/07/cloud-based-cryptocurrency-miners.html" target="_blank"> Cloud-based Cryptocurrency Miners Targeting GitHub Actions and Azure VMs </a> \
 </h3> \
 <small class="text-uppercase font-weight-bold">  \
 <span>July 11, 2022</span></small> \
