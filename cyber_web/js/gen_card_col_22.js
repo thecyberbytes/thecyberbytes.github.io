f8c9c90e292ecf20e5f4b5f3a52b9a03bb7aed1c
GitHub Action <mailcyberbytes@gmail.com>
2022-10-03 06:39:51 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_22.js
+++ cyber_web/js/gen_card_col_22.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/best-vpn/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/5e586f50ffd9380009895d8d/master/pass/feature_art-best_vpn.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 26, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The Best VPNs to Protect Yourself Online</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/2022-itu-secretary-general-election/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/632e50efc9bb1af7ef24988d/master/pass/UN-Vote-ITU-Security-GettyImages-1335882101.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 26, 2022</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">This Vote Could Change the Course of Internet History</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/best-vpn/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/5e586f50ffd9380009895d8d/master/pass/feature_art-best_vpn.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 26, 2022</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Best VPNs to Protect Yourself Online</h5> \
 </div> \
 </a> \
 </div> \
