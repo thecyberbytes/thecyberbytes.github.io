e2bf4200a8a3ac1ab77a48d915037c13385188e2
GitHub Action <mailcyberbytes@gmail.com>
2022-10-05 06:43:18 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_24.js
+++ cyber_web/js/gen_card_col_24.js
@@ -18,11 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/chainguard-wolfi-linux-distribution/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/632bbd9cd884b4dcc941656d/master/pass/Security-Open-Source-Linux-1403599186.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/covenant-eyes-anti-porn-accountability-monitoring-apps/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/632ba19662435d269fdbdba4/master/pass/security-shameware.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 22, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">A New Linux Tool Aims to Guard Against Supply Chain Attacks</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Ungodly Surveillance of Anti-Porn ‘Shameware’ Apps</h5> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -18,11 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/how-to-use-duckduckgo-privacy-first-email/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6324febb20bc56769c41311a/master/pass/How-to-Use-DuckDuckGo-Email-Privacy-Gear.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/queen-elizabeth-ii-funeral-details-security/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/63250dc8eecde8c117e7f4a4/master/pass/Queen-Queues-London-Security-GettyImages-1243218331.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 18, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">How to Use DuckDuckGo’s Privacy-First Email Service</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Queen’s Funeral Sets Off the Biggest UK Police Operation Ever</h5> \
 </div> \
 </a> \
 </div> \
