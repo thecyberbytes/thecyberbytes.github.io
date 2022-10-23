a1348ade66989cb13f094601d8968e8d331908f5
GitHub Action <mailcyberbytes@gmail.com>
2022-10-23 06:36:00 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_34.js
+++ cyber_web/js/gen_card_col_34.js
@@ -1,10 +1,10 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/windows-11-automatic-phishing-protection/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6340978f3099da4d4e0dd65f/master/pass/mail_windows11_sec_GettyImages-1349042028.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://nakedsecurity.sophos.com/2022/10/10/serious-security-oauth-2-and-why-microsoft-is-finally-forcing-you-into-it/" target="_blank" style="text-decoration:none"> \
+<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/10/oauth2-1200.png?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>October 10, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Windows 11 Now Offers Automatic Phishing Protection</h5> \
+<small class="text-uppercase font-weight-bold"><span>Vulnerability</span> <span class="mx-1">&bullet;</span> <span>October 10, 2022</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Serious Security: OAuth 2 and why Microsoft is finally forcing you into it</h5> \
 </div> \
 </a> \
 </div> \
