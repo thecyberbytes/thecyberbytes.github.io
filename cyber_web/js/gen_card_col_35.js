a4624cdea23f14e4b53bd2679f3586f66a112080
GitHub Action <mailcyberbytes@gmail.com>
2022-10-31 06:29:09 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -22,7 +22,7 @@ document.write(' \
 <img src="https://www.welivesecurity.com/wp-content/uploads/2022/10/zelle-scams-fraud-p2p-payment-apps.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>October 12, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">How scammers target Zelle users – and how you can stay safe</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">10 common Zelle scams – and how to avoid them</h5> \
 </div> \
 </a> \
 </div> \
