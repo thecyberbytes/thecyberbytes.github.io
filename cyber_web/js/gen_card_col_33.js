927de7eabc12626f7121c39519c06514abbce1f3
GitHub Action <mailcyberbytes@gmail.com>
2022-11-06 06:25:46 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_33.js
+++ cyber_web/js/gen_card_col_33.js
@@ -19,10 +19,10 @@ document.write(' \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://nakedsecurity.sophos.com/2022/10/24/serious-security-you-cant-beat-the-house-at-blackjack-or-can-you/" target="_blank" style="text-decoration:none"> \
-<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/10/card-fan-1200-1.jpg?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
+<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/10/bj-1200.jpg?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>October 24, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Serious Security: How randomly (or not) can you shuffle cards?</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Serious Security: You can’t beat the house at Blackjack – or can you?</h5> \
 </div> \
 </a> \
 </div> \
