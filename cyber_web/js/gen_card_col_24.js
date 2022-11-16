9be73b68672fa2ba0d348ca5a7c77624abf523eb
GitHub Action <mailcyberbytes@gmail.com>
2022-11-16 03:41:14 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_24.js
+++ cyber_web/js/gen_card_col_24.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://nakedsecurity.sophos.com/2022/11/03/s3-ep107-eight-months-to-kick-out-the-crooks-and-you-think-thats-good-audio-text/" target="_blank" style="text-decoration:none"> \
-<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/06/bn-1200.png?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/soccer-world-cup-biometric-surveillance/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/63630bb699fe777052767ac3/master/pass/Soccer-Fans,-You%E2%80%99re-Being-Watched-Security-GettyImages-1235954583.png" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 03, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">S3 Ep107: Eight months to kick out the crooks and you think that’s GOOD? [Audio + Text]</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Soccer Fans, You\'re Being Watched</h5> \
 </div> \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/submarine-internet-cables-egypt/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6361b2a71341ae5cc2594c4b/master/pass/The-Most-Vulnerable-Place-on-the-Internet-Security-GettyImages-1362710800.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/rust-secure-programming-language-memory-safe/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/63586e9551d3e1e46809effd/master/pass/Security-rust-programming.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 02, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The Most Vulnerable Place on the Internet</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Rise of Rust, the ‘Viral’ Secure Programming Language That’s Taking Over Tech</h5> \
 </div> \
 </a> \
 </div> \
