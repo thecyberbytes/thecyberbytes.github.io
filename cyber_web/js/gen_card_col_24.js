16c96e14f8cea6fad9d22a2671939ba9d9c9f7ef
GitHub Action <mailcyberbytes@gmail.com>
2022-11-20 19:04:45 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_24.js
+++ cyber_web/js/gen_card_col_24.js
@@ -18,11 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/silk-road-bitcoin-seizure-james-zhong/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/636958c63505151fbc0b01cb/master/pass/New-Silk-Road-Bitcoin-Bust-Security-GettyImages-653852096.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://nakedsecurity.sophos.com/2022/11/07/public-url-scanning-tools-when-security-leads-to-insecurity/" target="_blank" style="text-decoration:none"> \
+<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/11/typewriter-1200.jpg?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Crypto Currency</span> <span class="mx-1">&bullet;</span> <span>November 07, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">IRS Seizes Another Silk Road Hacker’s $3.36 Billion Bitcoin Stash</h5> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 07, 2022</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Public URL scanning tools – when security leads to insecurity</h5> \
 </div> \
 </a> \
 </div> \
