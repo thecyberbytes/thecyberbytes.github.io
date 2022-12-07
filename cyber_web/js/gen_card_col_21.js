189d902e78bfde93ec7bedc27cdcdcdc197df9bd
GitHub Action <mailcyberbytes@gmail.com>
2022-12-07 03:13:58 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_21.js
+++ cyber_web/js/gen_card_col_21.js
@@ -19,7 +19,7 @@ document.write(' \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://nakedsecurity.sophos.com/2022/12/01/the-christma-exec-network-worm-35-years-and-counting/" target="_blank" style="text-decoration:none"> \
-<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/12/xmas-1200.jpg?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
+<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/12/xmas-35-1200.png?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Hacking</span> <span class="mx-1">&bullet;</span> <span>December 01, 2022</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">The CHRISTMA EXEC network worm – 35 years and counting!</h5> \
