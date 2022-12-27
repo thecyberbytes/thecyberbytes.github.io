f0803a7137580126aaeca399f5c25acdbeac7251
GitHub Action <mailcyberbytes@gmail.com>
2022-12-27 03:07:10 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_13.js
+++ cyber_web/js/gen_card_col_13.js
@@ -13,11 +13,11 @@ document.write(' \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/modified-elephant-stan-swamy-hacked-evidence-frame-bhima-koregaon-16/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/63988e7ba8443cd37abeb320/master/pass/Swamy_arrest_India_Sec_GettyImages-1025314136.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/microsoft-certificates-ransomware-cuba-malware/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6398c9a94ea975d4889a94e7/master/pass/Microsoft-Cuba-Hacker-Gang-Security-GettyImages-1266759350.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 13, 2022</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Hackers Planted Files to Frame Indian Priest Who Died in Custody</h2> \
+<small class="text-uppercase font-weight-bold"><span>Ransomware</span> <span class="mx-1">&bullet;</span> <span>December 13, 2022</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">Cuba Ransomware Gang Abused Microsoft Certificates to Sign Malware</h2> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_25.js
+++ cyber_web/js/gen_card_col_25.js
@@ -1,10 +1,10 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/twitter-files-qanon-conspiracy-theories/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/63913887693cb0d7db268d6f/master/pass/Elon-Musk%E2%80%99s-Twitter-Files-Are-a-Feast-for-Conspiracy-Theorists-Security-GettyImages-1272694382.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://nakedsecurity.sophos.com/2022/12/08/credit-card-skimming-the-long-and-winding-road-of-supply-chain-failure/" target="_blank" style="text-decoration:none"> \
+<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/12/lawr-1200.jpg?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 08, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Elon Musk’s Twitter Files Are a Feast for Conspiracy Theorists</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Credit card skimming – the long and winding road of supply chain failure</h5> \
 </div> \
 </a> \
 </div> \
