b83581652e1da44800c913aeea5c39a03419fb7f
GitHub Action <mailcyberbytes@gmail.com>
2023-01-15 19:03:47 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_14.js
+++ cyber_web/js/gen_card_col_14.js
@@ -13,11 +13,11 @@ document.write(' \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://nakedsecurity.sophos.com/2022/12/29/us-passes-the-quantum-computing-cybersecurity-preparedness-act-and-why-not/" target="_blank" style="text-decoration:none"> \
-<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/12/sc-daa-1200-1.png?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.welivesecurity.com/videos/cybersecurity-trends-challenges-look-out-2023/" target="_blank" style="text-decoration:none"> \
+<img src="https://www.welivesecurity.com/wp-content/uploads/2022/12/0-39.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 29, 2022</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">US passes the Quantum Computing Cybersecurity Preparedness Act – and why not?</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">Cybersecurity trends and challenges to look out for in 2023</h2> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -13,7 +13,7 @@ document.write(' \
 <img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/12/vault-1200.jpg?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 23, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">LastPass finally admits: They did steal your password vaults after all</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">LastPass finally admits: Those crooks who got in? They did steal your password vaults, after all…</h5> \
 </div> \
 </a> \
 </div> \
@@ -22,7 +22,7 @@ document.write(' \
 <img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2022/12/vault-1200.jpg?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 23, 2022</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">LastPass finally admits: Those crooks who got in? They did steal your password vaults, after all…</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">LastPass finally admits: They did steal your password vaults after all</h5> \
 </div> \
 </a> \
 </div> \
