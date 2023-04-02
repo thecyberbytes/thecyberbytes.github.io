2dfee44cd23ab83c812becfeb46fadea269c9030
GitHub Action <mailcyberbytes@gmail.com>
2023-04-02 03:06:31 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_15.js
+++ cyber_web/js/gen_card_col_15.js
@@ -2,11 +2,11 @@ document.write(' \
 <div class="container-fluid border border-dark border-0 p-0 m-0 align-items-center justify-content-between"> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://nakedsecurity.sophos.com/2023/03/21/google-pixel-phones-had-a-serious-data-leakage-bug-heres-what-to-do/" target="_blank" style="text-decoration:none"> \
-<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2023/03/allthat-1200.png?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/how-to-tell-fake-ai-images-donald-trump-arrest/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/641a3cc41b037831fcc7504c/master/pass/How-You-Can-Tell-the-AI-Images-of-Trump%E2%80%99s-Arrest-Are-Deepfakes-Security-GettyImages-1248481192.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Data Breach</span> <span class="mx-1">&bullet;</span> <span>March 21, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Google Pixel phones had a serious data leakage bug – here’s what to do!</h2> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 21, 2023</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">How You Can Tell the AI Images of Trump’s Arrest Are Deepfakes</h2> \
 </div> \
 </a> \
 </div> \
