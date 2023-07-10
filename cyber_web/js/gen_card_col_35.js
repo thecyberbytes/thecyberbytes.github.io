1dd3afc86d924a22ac2dba9492bdf07c6f18d134
GitHub Action <mailcyberbytes@gmail.com>
2023-07-10 03:22:51 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -1,10 +1,10 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://nakedsecurity.sophos.com/2023/06/21/beware-bad-passwords-as-attackers-co-opt-linux-servers-into-cybercrime/" target="_blank" style="text-decoration:none"> \
-<img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2023/06/lan-1200.jpg?w=230&h=130&crop=1" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.welivesecurity.com/2023/06/21/vacation-stay-step-ahead-travel-scammers/" target="_blank" style="text-decoration:none"> \
+<img src="https://www.welivesecurity.com/wp-content/uploads/2023/06/vacation-cybercrime-threats.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>June 21, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Beware bad passwords as attackers co-opt Linux servers into cybercrime</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Going on vacation soon? Stay one step ahead of travel scammers</h5> \
 </div> \
 </a> \
 </div> \
