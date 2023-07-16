0b8ace9b5759ff4e39b77a2571519d4ac4216049
GitHub Action <mailcyberbytes@gmail.com>
2023-07-16 19:03:37 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_32.js
+++ cyber_web/js/gen_card_col_32.js
@@ -22,7 +22,7 @@ document.write(' \
 <img src="https://media.wired.com/photos/64a6f2af0cbc15924e602d94/master/pass/Don\'t-Join-Threads-Security-GettyImages-1508798453.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>July 06, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Don\'t Join Threads—Make Instagram\'s \'Twitter Killer\' Join You</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Don\'t Join Threads—Make Meta\'s Instagram-Linked Twitter Alternative Join You</h5> \
 </div> \
 </a> \
 </div> \
