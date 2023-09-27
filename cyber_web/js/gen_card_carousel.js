1e9600c307ce91506d93a2d94717af8d34df59b1
GitHub Action <mailcyberbytes@gmail.com>
2023-09-27 19:03:56 +0000

    Commiting Category data

--- cyber_web/js/gen_card_carousel.js
+++ cyber_web/js/gen_card_carousel.js
@@ -1,5 +1,23 @@
 document.write(' \
 <div class="carousel-item active"> \
+<a href="https://www.wired.com/story/soundthinking-geolitica-acquisition-predictive-policing/" target="_blank"> \
+<img src="https://media.wired.com/photos/65136b6ae49d26d715b01988/master/pass/ShotSpot_Final.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<div class="carousel-caption"> \
+<h2>SoundThinking, Maker of ShotSpotter, Is Buying Parts of PredPol Creator Geolitica</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">September 27, 2023</p> \
+</div> \
+</a> \
+</div> \
+<div class="carousel-item "> \
+<a href="https://krebsonsecurity.com/2023/09/snatch-ransom-group-exposes-visitor-ip-addresses/" target="_blank"> \
+<img src="https://krebsonsecurity.com/wp-content/uploads/2023/09/snatchhome.png" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<div class="carousel-caption"> \
+<h2>‘Snatch’ Ransom Group Exposes Visitor IP Addresses</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">September 27, 2023</p> \
+</div> \
+</a> \
+</div> \
+<div class="carousel-item "> \
 <a href="https://www.wired.com/story/us-government-shutdown-right-wing-bubble/" target="_blank"> \
 <img src="https://media.wired.com/photos/6511d2cd8122e64edaa87efa/master/pass/The-Right-Wing-Bubble-That-Could-Shut-Down-the-US-Government-Security-GettyImages-1542489918.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
 <div class="carousel-caption"> \
@@ -14,24 +32,6 @@ document.write(' \
 <div class="carousel-caption"> \
 <h2>Update on Naked Security</h2> \
 <p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">September 26, 2023</p> \
-</div> \
-</a> \
-</div> \
-<div class="carousel-item "> \
-<a href="https://www.wired.com/story/corporate-surveillance-train-ai/" target="_blank"> \
-<img src="https://media.wired.com/photos/64b1d28d3d9e6b1cb17a8a00/master/pass/How-to-Use-Generative-AI-Tools-While-Still-Protecting-Your-Privacy-Security.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
-<div class="carousel-caption"> \
-<h2>Your Boss’s Spyware Could Train AI to Replace You</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">September 25, 2023</p> \
-</div> \
-</a> \
-</div> \
-<div class="carousel-item "> \
-<a href="https://www.wired.com/story/equalitie-trojan-horse-internet-censorship/" target="_blank"> \
-<img src="https://media.wired.com/photos/650def222839bb922141791c/master/pass/A-Tricky-New-Way-to-Sneak-Past-Repressive-Internet-Censorship-Security-GettyImages-502301697.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
-<div class="carousel-caption"> \
-<h2>A Tricky New Way to Sneak Past Repressive Internet Censorship</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">September 25, 2023</p> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_32.js
+++ cyber_web/js/gen_card_col_32.js
@@ -1,5 +1,14 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/trickbot-conti-sanctions-indictments/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/64fa184b406fe6679c3e7755/master/pass/US-and-UK-Mount-Aggressive-Crackdown-on-Trickbot-and-Conti-Ransomware-Gangs-Security-GettyImages-502794865.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Ransomware</span> <span class="mx-1">&bullet;</span> <span>September 07, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">US and UK Mount Aggressive Crackdown on Trickbot and Conti Ransomware Gangs</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/icc-cyberwar-crimes/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/64f91bbcd07a0ebe6852d81f/master/pass/The-Hague\'s-Lead-Prosecutor-Says-He\'ll-Now-Charge-Cyberwar-Crimes-Security-GettyImages-1258493757.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
@@ -9,20 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/generative-ai-prompt-injection-hacking/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/64f785e96b7fc716358e649b/master/pass/race-to-fix-ai\'s-biggest-security-flaw-Security-GettyImages-1357644507.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/china-backed-hackers-steal-microsofts-signing-key-post-mortem/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/64f8dff9e5005395292347e0/master/pass/The-Comedy-of-Errors-That-Let-China-Backed-Hackers-Steal-Microsoft%E2%80%99s-Signing-Key-Security-GettyImages-1272742253.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 06, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Generative AI’s Biggest Security Flaw Is Not Easy to Fix</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/china-vulnerability-disclosure-law/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/64f40f94c6fd2e04ef71a3cf/master/pass/090123-china-software-vulnerabilities.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 06, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">How China Demands Tech Firms Reveal Hackable Flaws in Their Products</h5> \
+<small class="text-uppercase font-weight-bold"><span>Vulnerability</span> <span class="mx-1">&bullet;</span> <span>September 07, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Comedy of Errors That Let China-Backed Hackers Steal Microsoft’s Signing Key</h5> \
 </div> \
 </a> \
 </div> \
