7c6867933a58c5809dec364e3b2e69083644e6e4
GitHub Action <mailcyberbytes@gmail.com>
2023-10-07 19:03:46 +0000

    Commiting Category data

--- cyber_web/js/gen_card_carousel.js
+++ cyber_web/js/gen_card_carousel.js
@@ -1,5 +1,23 @@
 document.write(' \
 <div class="carousel-item active"> \
+<a href="https://www.wired.com/story/active-clubs-telegram/" target="_blank"> \
+<img src="https://media.wired.com/photos/651d7d34638c0702bea2d768/master/pass/Tennessee-Active-Club-Security-GettyImages-1212813932.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<div class="carousel-caption"> \
+<h2>White Supremacist Active Clubs Are Breeding on Telegram</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">October 07, 2023</p> \
+</div> \
+</a> \
+</div> \
+<div class="carousel-item "> \
+<a href="https://www.wired.com/story/apple-heat-initiative-dark-money/" target="_blank"> \
+<img src="https://media.wired.com/photos/65203989ee2aae07a8f08a84/master/pass/Apple\'s-Encryption-Is-Under-Attack-by-a-Mysterious-Group-Security-GettyImages-1235762749.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<div class="carousel-caption"> \
+<h2>Apple\'s Encryption Is Under Attack by a Mysterious Group</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">October 07, 2023</p> \
+</div> \
+</a> \
+</div> \
+<div class="carousel-item "> \
 <a href="https://www.wired.com/story/23andme-credential-stuffing-data-stolen/" target="_blank"> \
 <img src="https://media.wired.com/photos/65207cec948b4a60f532e175/master/pass/23andMe-Data-Breach-Security-GettyImages-1230824354.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
 <div class="carousel-caption"> \
@@ -9,28 +27,10 @@ document.write(' \
 </a> \
 </div> \
 <div class="carousel-item "> \
-<a href="https://www.wired.com/story/the-experts-helping-women-fight-digital-domestic-abuse/" target="_blank"> \
-<img src="https://media.wired.com/photos/651c4152ff50c54b5a401580/master/pass/EmmaPickering_07.14_AK_026_CCISO.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
-<div class="carousel-caption"> \
-<h2>The Team Helping Women Fight Digital Domestic Abuse</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">October 04, 2023</p> \
-</div> \
-</a> \
-</div> \
-<div class="carousel-item "> \
 <a href="https://www.wired.com/story/android-tv-streaming-boxes-china-backdoor/" target="_blank"> \
 <img src="https://media.wired.com/photos/651ca80b3f56c202434baa74/master/pass/Your-Cheap-Android-TV-Streaming-Box-May-Have-a-Dangerous-Backdoor-Security-GettyImages-539478880.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
 <div class="carousel-caption"> \
 <h2>Your Cheap Android TV Streaming Box May Have a Dangerous Backdoor</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">October 04, 2023</p> \
-</div> \
-</a> \
-</div> \
-<div class="carousel-item "> \
-<a href="https://www.wired.com/story/neuralink-uc-davis-monkey-photos-videos-secret/" target="_blank"> \
-<img src="https://media.wired.com/photos/6517391e6b593d112edbb6c1/master/pass/091923-security-elon-musk-neuralink-testing-secret-1.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
-<div class="carousel-caption"> \
-<h2>How Neuralink Keeps Dead Monkey Photos Secret</h2> \
 <p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">October 04, 2023</p> \
 </div> \
 </a> \
--- cyber_web/js/gen_card_col_14.js
+++ cyber_web/js/gen_card_col_14.js
@@ -2,22 +2,22 @@ document.write(' \
 <div class="container-fluid border border-dark border-0 p-0 m-0 align-items-center justify-content-between"> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/axon-taser-drones-ethics-board/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/64fb54d7b62b2d78a960d713/master/pass/art_science-taser-drone-art_final.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/local-governments-generative-ai/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/64fbb7617ced5356bad14626/master/pass/AI-Chatbots-Are-Invading-Your-Local-Government%E2%80%94and-Making-Everyone-Nervous-Security-GettyImages-1384459158.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 08, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Axon\'s Ethics Board Resigned Over Taser-Armed Drones. Then the Company Bought a Military Drone Maker</h2> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 11, 2023</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">AI Chatbots Are Invading Your Local Government—and Making Everyone Nervous</h2> \
 </div> \
 </a> \
 </div> \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/section-702-privacy-meeting/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/64fb816788d1d593902bec22/master/pass/Top-US-Spies-Meet-With-Privacy-Experts-Over-Surveillance-%E2%80%98Crown-Jewel%E2%80%99-Security-GettyImages-1383965396.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/your-new-car-privacy-nightmare/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/64fb99290b30a608b5a972a1/master/pass/Your-New-Car-Is-a-Privacy-Nightmare-Security-GettyImages-88622688.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>September 08, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Top US Spies Meet With Privacy Experts Over Surveillance \'Crown Jewel\'</h2> \
+<small class="text-uppercase font-weight-bold"><span>Data Breach</span> <span class="mx-1">&bullet;</span> <span>September 09, 2023</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">Mozilla: Your New Car Is a Data Privacy Nightmare</h2> \
 </div> \
 </a> \
 </div> \
