c4efd5554afa653d9ea47773dccf2c00ab35c206
GitHub Action <mailcyberbytes@gmail.com>
2023-10-22 03:06:59 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_23.js
+++ cyber_web/js/gen_card_col_23.js
@@ -1,5 +1,14 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/the-experts-helping-women-fight-digital-domestic-abuse/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/651c4152ff50c54b5a401580/master/pass/EmmaPickering_07.14_AK_026_CCISO.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>October 04, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Team Helping Women Fight Digital Domestic Abuse</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/android-tv-streaming-boxes-china-backdoor/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/651ca80b3f56c202434baa74/master/pass/Your-Cheap-Android-TV-Streaming-Box-May-Have-a-Dangerous-Backdoor-Security-GettyImages-539478880.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
@@ -14,15 +23,6 @@ document.write(' \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>October 04, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">How Neuralink Keeps Dead Monkey Photos Secret</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/the-experts-helping-women-fight-digital-domestic-abuse/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/651c4152ff50c54b5a401580/master/pass/EmmaPickering_07.14_AK_026_CCISO.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>October 04, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The Team Helping Women Fight Digital Domestic Abuse</h5> \
 </div> \
 </a> \
 </div> \
