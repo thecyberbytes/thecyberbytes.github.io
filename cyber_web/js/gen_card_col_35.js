ab21ddfe339d8720bd1be4b9506a83081a5faa38
GitHub Action <mailcyberbytes@gmail.com>
2023-10-30 19:03:41 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -1,19 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/phone-ios-android-security-updates/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65175c3fcb3588ab805640b9/master/pass/How-To-Phone-Security-Updates-Security-595478851.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>October 01, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">How to Tell When Your Phone Will Stop Getting Security Updates</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/google-bard-location-data-tracking-ai/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/65175aef0de7768141220d4a/master/pass/Google-Bard-Data-Removal-Security-1124778123.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Data Breach</span> <span class="mx-1">&bullet;</span> <span>October 01, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">How to Stop Google Bard From Storing Your Data and Location</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/phone-ios-android-security-updates/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65175c3fcb3588ab805640b9/master/pass/How-To-Phone-Security-Updates-Security-595478851.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>October 01, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">How to Tell When Your Phone Will Stop Getting Security Updates</h5> \
 </div> \
 </a> \
 </div> \
