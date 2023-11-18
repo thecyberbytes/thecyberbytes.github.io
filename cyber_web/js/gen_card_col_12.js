f0aa26e9d13bfc59af903772448f8f154ef365ea
GitHub Action <mailcyberbytes@gmail.com>
2023-11-18 03:07:17 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_12.js
+++ cyber_web/js/gen_card_col_12.js
@@ -13,11 +13,11 @@ document.write(' \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/uk-police-face-recognition-expansion/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/654c2c7f0202a11bfdc62856/master/pass/The-UK-Is-Turning-Into-a-Facial-Recognition-Police-State-Security-GettyImages-1224837570.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/omegle-shutdown-lawsuit-child-sexual-abuse/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/654d0f4af3c4c30aa39e1952/master/pass/Omegle-Should-Have-Died-Years-Ago-Security-GettyImages-1137810811.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Police Use of Face Recognition Is Sweeping the UK</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">Omegle Was Forced to Shut Down by a Lawsuit From a Sexual Abuse Survivor</h2> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_13.js
+++ cyber_web/js/gen_card_col_13.js
@@ -13,11 +13,11 @@ document.write(' \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/san-francisco-doom-loop-citizen-surveillance/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/654271e660c83bd9a94a4c10/master/pass/LEDE-231021-IanBates--409.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/israel-gaza-internet-blackouts-weapon/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6549a751ffe219707629bcb0/master/pass/Communication-Towers-Gaza-Security-1749945926.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 07, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">What a Bloody San Francisco Street Brawl Tells Us About the Age of Citizen Surveillance</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">Internet Blackouts in Gaza Are a New Weapon in the Israel-Hamas War</h2> \
 </div> \
 </a> \
 </div> \
