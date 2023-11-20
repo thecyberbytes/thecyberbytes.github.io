4e20a2b185b4970076b456fd8a565a5d8e0e167a
GitHub Action <mailcyberbytes@gmail.com>
2023-11-20 03:07:45 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_22.js
+++ cyber_web/js/gen_card_col_22.js
@@ -1,10 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/section-702-nsa-surveillance-senate/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/654e9bd2487e919418959672/master/pass/US-Senate-Leaders-Plan-to-Extend-Surveillance-Program-Using-Federal-Budget-Security-GettyImages-1756847694.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/altitude-terrorism-content-removal-tool/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/654d5cecead917fc613de499/master/pass/This-New-Tool-Aims-To-Help-Keep-Terror-Content-Off-The-Internet-Security-GettyImages-1210458796.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 10, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Senate Leaders Plan to Prolong NSA Surveillance Using a Must-Pass Bill</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">This New Tool Aims to Keep Terrorism Content Off the Internet</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/how-to-get-ad-free-facebook-instagram-meta-privacy/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/654568cd555ea37fb2a4b3f3/master/pass/Use-Facebook-without-Ads-Security-GettyImages-1200899039.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">How to Get Facebook Without Ads—if It’s Available for You</h5> \
 </div> \
 </a> \
 </div> \
@@ -14,15 +23,6 @@ document.write(' \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Omegle Was Forced to Shut Down by a Lawsuit From a Sexual Abuse Survivor</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/uk-police-face-recognition-expansion/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/654c2c7f0202a11bfdc62856/master/pass/The-UK-Is-Turning-Into-a-Facial-Recognition-Police-State-Security-GettyImages-1224837570.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Police Use of Face Recognition Is Sweeping the UK</h5> \
 </div> \
 </a> \
 </div> \
