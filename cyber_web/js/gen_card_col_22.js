6d3e60024eebb278bf6d101c3ae1a4d0e78e0d0b
GitHub Action <mailcyberbytes@gmail.com>
2023-11-21 19:04:32 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_22.js
+++ cyber_web/js/gen_card_col_22.js
@@ -18,11 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/how-to-get-ad-free-facebook-instagram-meta-privacy/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/654568cd555ea37fb2a4b3f3/master/pass/Use-Facebook-without-Ads-Security-GettyImages-1200899039.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/sandworm-ukraine-third-blackout-cyberattack/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/654c0c0cf216c1860ca45fef/master/pass/Sandworm-Hackers-Caused-Another-Blackout-in-Ukraine%E2%80%94During-a-Missile-Strike-Security-GettyImages-1244660782.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">How to Get Facebook Without Ads—if It’s Available for You</h5> \
+<small class="text-uppercase font-weight-bold"><span>Vulnerability</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Sandworm Hackers Caused Another Blackout in Ukraine—During a Missile Strike</h5> \
 </div> \
 </a> \
 </div> \
