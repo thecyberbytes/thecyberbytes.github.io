13cd49810fe62f508e2beea22dce01b7369d8c92
GitHub Action <mailcyberbytes@gmail.com>
2023-11-22 19:04:21 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_32.js
+++ cyber_web/js/gen_card_col_32.js
@@ -1,10 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/generative-ai-terrorism-content/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/654d640f71250da0bf23bfca/master/pass/security_extremists_ai.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/uk-police-face-recognition-expansion/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/654c2c7f0202a11bfdc62856/master/pass/The-UK-Is-Turning-Into-a-Facial-Recognition-Police-State-Security-GettyImages-1224837570.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Here’s How Violent Extremists Are Exploiting Generative AI Tools</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Police Use of Face Recognition Is Sweeping the UK</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/omegle-shutdown-lawsuit-child-sexual-abuse/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/654d0f4af3c4c30aa39e1952/master/pass/Omegle-Should-Have-Died-Years-Ago-Security-GettyImages-1137810811.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Omegle Was Forced to Shut Down by a Lawsuit From a Sexual Abuse Survivor</h5> \
 </div> \
 </a> \
 </div> \
@@ -14,15 +23,6 @@ document.write(' \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">How to Get Facebook Without Ads—if It’s Available for You</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/uk-police-face-recognition-expansion/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/654c2c7f0202a11bfdc62856/master/pass/The-UK-Is-Turning-Into-a-Facial-Recognition-Police-State-Security-GettyImages-1224837570.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 09, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Police Use of Face Recognition Is Sweeping the UK</h5> \
 </div> \
 </a> \
 </div> \
