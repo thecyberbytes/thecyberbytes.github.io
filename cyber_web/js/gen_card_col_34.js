ad5985e276617b4067547b2b262e46f83907fe0a
GitHub Action <mailcyberbytes@gmail.com>
2023-11-24 03:07:22 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_34.js
+++ cyber_web/js/gen_card_col_34.js
@@ -1,19 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/culturepulse-ai-israeli-palestinian-crisis/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6542d6d8a40bbd76f142b097/master/pass/The-UN-Hired-an-AI-Company-to-Untangle-the-Israeli-Palestinian-Crisis-Security-GettyImages-1714963353.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 02, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The UN Hired an AI Company to Untangle the Israeli-Palestinian Crisis</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/microsoft-secure-future-initiative/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/6542ce96ef12892fe0b649cf/master/pass/Microsoft-Security-GettyImages-1207074415.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 02, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Microsoft Does Damage Control With Its New \'Secure Future Initiative\'</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/culturepulse-ai-israeli-palestinian-crisis/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6542d6d8a40bbd76f142b097/master/pass/The-UN-Hired-an-AI-Company-to-Untangle-the-Israeli-Palestinian-Crisis-Security-GettyImages-1714963353.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 02, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The UN Hired an AI Company to Untangle the Israeli-Palestinian Crisis</h5> \
 </div> \
 </a> \
 </div> \
