cb6626eb9148fe7558826ae73fcf9edc2d8dae3a
GitHub Action <mailcyberbytes@gmail.com>
2023-12-10 03:07:13 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_12.js
+++ cyber_web/js/gen_card_col_12.js
@@ -2,22 +2,22 @@ document.write(' \
 <div class="container-fluid border border-dark border-0 p-0 m-0 align-items-center justify-content-between"> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/india-radio-elections-narendra-modi/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6565f481f082e4efb66daf97/master/pass/Radio-Waves-India-Business-1401047378.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/elon-musk-qanon-2024-election/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6564a7ac38b869716d5be546/master/pass/QAnon-politics-GettyImages-1244812857.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 28, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">The Hundred-Year Battle for India’s Radio Airwaves</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">Elon Musk Is Giving QAnon Believers Hope Just in Time for the 2024 Elections</h2> \
 </div> \
 </a> \
 </div> \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/telegram-hamas-channels-deplatform/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6557c4dc65f11bb1e627563e/master/pass/Telegram_Lies_Final_Z.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/india-radio-elections-narendra-modi/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6565f481f082e4efb66daf97/master/pass/Radio-Waves-India-Business-1401047378.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 28, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Telegram’s Bans on Extremist Channels Aren\'t Really Bans</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">The Hundred-Year Battle for India’s Radio Airwaves</h2> \
 </div> \
 </a> \
 </div> \
