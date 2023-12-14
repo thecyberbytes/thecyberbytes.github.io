9868f4ec36a30009f41c238abd29267c93a83a1e
GitHub Action <mailcyberbytes@gmail.com>
2023-12-14 03:07:29 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_14.js
+++ cyber_web/js/gen_card_col_14.js
@@ -2,11 +2,11 @@ document.write(' \
 <div class="container-fluid border border-dark border-0 p-0 m-0 align-items-center justify-content-between"> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/take-down-trump-truth-social/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65559e6efcab296c7daee707/master/pass/Truth-Social-Takedown-Politics-1431869546.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/usa-binance-money-laundering-charges/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/655cfa1ebca7c5da5ada0a06/master/pass/Binance-Criminal-Charges-Security-2MAARRT.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 21, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Inside the Operation to Bring Down Trump’s Truth Social</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">DOJ Charges Binance With Vast Money-Laundering Scheme and Sanctions Violations</h2> \
 </div> \
 </a> \
 </div> \
