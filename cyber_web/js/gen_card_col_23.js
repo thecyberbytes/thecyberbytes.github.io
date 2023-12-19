f26990f9b2253eb0aa727e6b33a7614dc2114b30
GitHub Action <mailcyberbytes@gmail.com>
2023-12-19 03:07:34 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_23.js
+++ cyber_web/js/gen_card_col_23.js
@@ -9,11 +9,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/india-radio-elections-narendra-modi/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6565f481f082e4efb66daf97/master/pass/Radio-Waves-India-Business-1401047378.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/elon-musk-qanon-2024-election/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6564a7ac38b869716d5be546/master/pass/QAnon-politics-GettyImages-1244812857.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 28, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The Hundred-Year Battle for India’s Radio Airwaves</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Elon Musk Is Giving QAnon Believers Hope Just in Time for the 2024 Elections</h5> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_31.js
+++ cyber_web/js/gen_card_col_31.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/section-702-immigrant-families-surveillance/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/656ddba48df40f19529e356c/master/pass/GettyImages-1173633491.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 04, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">US Lawmakers Want to Use a Powerful Spy Tool on Immigrants and Their Families</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/inside-americas-school-internet-censorship-machine/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/656a385cb4bd4ee94b9f456a/master/pass/security_school_internet_censorship_1268517553.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 04, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Inside America\'s School Internet Censorship Machine</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/section-702-immigrant-families-surveillance/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/656ddba48df40f19529e356c/master/pass/GettyImages-1173633491.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 04, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">US Lawmakers Want to Use a Powerful Spy Tool on Immigrants and Their Families</h5> \
 </div> \
 </a> \
 </div> \
