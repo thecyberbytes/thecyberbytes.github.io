47d1e41b50aaf77205f40867abe777d8d64eefa4
GitHub Action <mailcyberbytes@gmail.com>
2023-12-20 19:03:25 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_13.js
+++ cyber_web/js/gen_card_col_13.js
@@ -13,11 +13,11 @@ document.write(' \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/openai-custom-chatbots-gpts-prompt-injection-attacks/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65668a1b4cb65084356e789a/master/pass/112823-security-openai-gpts-data.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/okta-breach-disclosure-all-customer-support-users/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65674025cbc8271e13d7317a/master/pass/okta-sec-2F8DR2R-(1).jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 29, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">OpenAI’s Custom Chatbots Are Leaking Their Secrets</h2> \
+<small class="text-uppercase font-weight-bold"><span>Data Breach</span> <span class="mx-1">&bullet;</span> <span>November 29, 2023</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">Okta Breach Impacted All Customer Support Users—Not 1 Percent</h2> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_23.js
+++ cyber_web/js/gen_card_col_23.js
@@ -1,10 +1,10 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/okta-breach-disclosure-all-customer-support-users/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65674025cbc8271e13d7317a/master/pass/okta-sec-2F8DR2R-(1).jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/openai-custom-chatbots-gpts-prompt-injection-attacks/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65668a1b4cb65084356e789a/master/pass/112823-security-openai-gpts-data.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Data Breach</span> <span class="mx-1">&bullet;</span> <span>November 29, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Okta Breach Impacted All Customer Support Users—Not 1 Percent</h5> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 29, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">OpenAI’s Custom Chatbots Are Leaking Their Secrets</h5> \
 </div> \
 </a> \
 </div> \
@@ -18,11 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/elon-musk-qanon-2024-election/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6564a7ac38b869716d5be546/master/pass/QAnon-politics-GettyImages-1244812857.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/2023-ndaa-privacy-lobbying/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65662d83fa25ced80447aaa1/master/pass/bill-housespeaker-sec-GettyImages-1763978971.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 28, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Elon Musk Is Giving QAnon Believers Hope Just in Time for the 2024 Elections</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">A Civil Rights Firestorm Erupts Around a Looming Surveillance Power Grab</h5> \
 </div> \
 </a> \
 </div> \
