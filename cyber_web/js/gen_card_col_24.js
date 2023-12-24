68cc9bfd3abe4c417ddb3c7e4e20e8fa9a566d1f
GitHub Action <mailcyberbytes@gmail.com>
2023-12-24 03:07:04 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_24.js
+++ cyber_web/js/gen_card_col_24.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/apple-iphone-namedrop-ios17/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6564abf23dbf6a669da7ae03/master/pass/sec-Apple-iOS-17-NameDrop.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 27, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">You Don’t Need to Turn Off Apple’s NameDrop Feature in iOS 17</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/section-702-reauthorization-ndaa-2023/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/6564f9eb971cdadbf317f3a0/master/pass/Mike-Johnson-Section-702-Security-1754110292.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 27, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Section 702 Surveillance Reauthorization May Get Slipped Into ‘Must-Pass’ NDAA</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/apple-iphone-namedrop-ios17/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6564abf23dbf6a669da7ae03/master/pass/sec-Apple-iOS-17-NameDrop.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 27, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">You Don’t Need to Turn Off Apple’s NameDrop Feature in iOS 17</h5> \
 </div> \
 </a> \
 </div> \
