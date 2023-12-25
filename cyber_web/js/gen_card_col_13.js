fb86990750cc2899aaebdca893553f6408e090f4
GitHub Action <mailcyberbytes@gmail.com>
2023-12-25 03:07:49 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_13.js
+++ cyber_web/js/gen_card_col_13.js
@@ -13,11 +13,11 @@ document.write(' \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/cdc-gun-violence-research-gop-proposal/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6567ae2b3e2f63900ec5d893/master/pass/CDC-guns-sec-GettyImages-1246717458.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/google-chrome-seven-zero-day-flaws-critical-update-november-2023/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6568aca70c0e7d175724ef99/master/pass/security_november_critical_update.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 30, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">The CDC\'s Gun Violence Research Is in Danger</h2> \
+<small class="text-uppercase font-weight-bold"><span>Vulnerability</span> <span class="mx-1">&bullet;</span> <span>November 30, 2023</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">Google Fixes a Seventh Zero-Day Flaw in Chrome—Update Now</h2> \
 </div> \
 </a> \
 </div> \
