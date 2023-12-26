304dc960509bc03cd1c2c31873e792e60b1dcbf9
GitHub Action <mailcyberbytes@gmail.com>
2023-12-26 03:07:33 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -1,19 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/indian-startup-hack-for-hire-security-roundup/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6557ed60591547359b3c7022/master/pass/The-Startup-That-Transformed-The-Hack-for-Hire-Industry-Security-GettyImages-685008639.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 18, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The Startup That Transformed the Hack-for-Hire Industry</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/f1-las-vegas-grand-prix-security/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/6557e61b8ba298d25d23d7f5/master/pass/Inside-The-Race-to-Secure-the-Formula-1-Las-Vegas-Grand-Prix-Security-GettyImages-1787267227.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 18, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Inside the Race to Secure the F1 Las Vegas Grand Prix</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/indian-startup-hack-for-hire-security-roundup/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6557ed60591547359b3c7022/master/pass/The-Startup-That-Transformed-The-Hack-for-Hire-Industry-Security-GettyImages-685008639.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>November 18, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Startup That Transformed the Hack-for-Hire Industry</h5> \
 </div> \
 </a> \
 </div> \
