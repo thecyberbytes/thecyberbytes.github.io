fdc0538977a4c90f1e40bbee9b42f7c39005e830
thecyberbytes <108113656+thecyberbytes@users.noreply.github.com>
2024-02-12 17:29:25 +1100

    Update feed_file_generator.py
    added 'User-Agent' in header for the request to medium feed (Forbidden error issue)

--- cyber_web/ml_code/feed_file_generator.py
+++ cyber_web/ml_code/feed_file_generator.py
@@ -10,11 +10,16 @@ warnings.filterwarnings('ignore')
 
 import requests
 from bs4 import BeautifulSoup
+import random
 
 def pull_data(url):
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
-
-    response = requests.get(url, headers=HEADERS)
+    user_agents_list = [
+        'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+    ]
+    
+    response = requests.get(url, headers={'User-Agent': random.choice(user_agents_list)})
     response.raise_for_status()
 
     content = response.content#resp.content.decode('utf8')
