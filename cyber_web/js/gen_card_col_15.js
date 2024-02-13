1aed362cfcc730eb38de4c3c9c970a299aeb69e2
GitHub Action <mailcyberbytes@gmail.com>
2024-02-13 19:03:57 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_15.js
+++ cyber_web/js/gen_card_col_15.js
@@ -2,22 +2,22 @@ document.write(' \
 <div class="container-fluid border border-dark border-0 p-0 m-0 align-items-center justify-content-between"> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/meta-messenger-instagram-end-to-end-encryption/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65713f1724772bae860336af/master/pass/Meta-End-To-End-Ecryption-Security-1365604441.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/elijah-wood-mike-tyson-russia-ukraine-disinformation/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6571f6e3e5736884fcbcc20b/master/pass/The-Kremlin-Is-on-Cameo-Security.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 07, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">End-to-End Encrypted Instagram and Messenger Chats: Why It Took Meta 7 Years</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">Elijah Wood and Mike Tyson Cameo Videos Were Used in a Russian Disinformation Campaign</h2> \
 </div> \
 </a> \
 </div> \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/elijah-wood-mike-tyson-russia-ukraine-disinformation/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6571f6e3e5736884fcbcc20b/master/pass/The-Kremlin-Is-on-Cameo-Security.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/meta-messenger-instagram-end-to-end-encryption/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65713f1724772bae860336af/master/pass/Meta-End-To-End-Ecryption-Security-1365604441.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 07, 2023</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Elijah Wood and Mike Tyson Cameo Videos Were Used in a Russian Disinformation Campaign</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">End-to-End Encrypted Instagram and Messenger Chats: Why It Took Meta 7 Years</h2> \
 </div> \
 </a> \
 </div> \
