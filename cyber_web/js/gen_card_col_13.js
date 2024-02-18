f6d9717d27164bc83f84b2d16072670abc3a0531
GitHub Action <mailcyberbytes@gmail.com>
2024-02-18 03:07:17 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_13.js
+++ cyber_web/js/gen_card_col_13.js
@@ -2,22 +2,22 @@ document.write(' \
 <div class="container-fluid border border-dark border-0 p-0 m-0 align-items-center justify-content-between"> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/appin-training-centers-lawsuits-censorship/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65b99af46599a951c10c510b/master/pass/security_indian_hackers_coverup.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/ftx-heist-sim-swat-indictment/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65bbff20098d608f3b8b686b/master/pass/FTX-Heist-Security-1244772036.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 01, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">A Startup Allegedly ‘Hacked the World.’ Then Came the Censorship—and Now the Backlash</h2> \
+<small class="text-uppercase font-weight-bold"><span>Hacking</span> <span class="mx-1">&bullet;</span> <span>February 01, 2024</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">The Mystery of the $400 Million FTX Heist May Have Been Solved</h2> \
 </div> \
 </a> \
 </div> \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/ftx-heist-sim-swat-indictment/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65bbff20098d608f3b8b686b/master/pass/FTX-Heist-Security-1244772036.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/appin-training-centers-lawsuits-censorship/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65b99af46599a951c10c510b/master/pass/security_indian_hackers_coverup.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Hacking</span> <span class="mx-1">&bullet;</span> <span>February 01, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">The Mystery of the $400 Million FTX Heist May Have Been Solved</h2> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 01, 2024</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">A Startup Allegedly ‘Hacked the World.’ Then Came the Censorship—and Now the Backlash</h2> \
 </div> \
 </a> \
 </div> \
