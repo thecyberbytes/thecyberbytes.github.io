80e9d131d98b2bdc9acf876b52f54c2bfaf31cb6
GitHub Action <mailcyberbytes@gmail.com>
2024-02-21 03:07:24 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_25.js
+++ cyber_web/js/gen_card_col_25.js
@@ -1,19 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/cybersecurity-industry-fbi-ransomware/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6557ed60591547359b3c7022/master/pass/The-Startup-That-Transformed-The-Hack-for-Hire-Industry-Security-GettyImages-685008639.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Ransomware</span> <span class="mx-1">&bullet;</span> <span>December 17, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Cybersecurity Industry Baffled by FBI’s Lack of Action on Ransomware Gang</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/indian-startup-hack-for-hire-security-roundup/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/6557ed60591547359b3c7022/master/pass/The-Startup-That-Transformed-The-Hack-for-Hire-Industry-Security-GettyImages-685008639.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>December 17, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">The Startup That Transformed the Hack-for-Hire Industry</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/cybersecurity-industry-fbi-ransomware/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6557ed60591547359b3c7022/master/pass/The-Startup-That-Transformed-The-Hack-for-Hire-Industry-Security-GettyImages-685008639.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Ransomware</span> <span class="mx-1">&bullet;</span> <span>December 17, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Cybersecurity Industry Baffled by FBI’s Lack of Action on Ransomware Gang</h5> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -1,5 +1,14 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/microsoft-digital-crime-unit-2023/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/657a0773d369f062c9dc83d8/master/pass/WorkingintheForensic_Print-1024x682-1024x682.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Vulnerability</span> <span class="mx-1">&bullet;</span> <span>December 14, 2023</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Microsoft’s Digital Crime Unit Goes Deep on How It Disrupts Cybercrime</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/kytch-taylor-mcdonalds-ice-cream-machine-smoking-gun/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/657a3fe5d50c518000e43e8c/master/pass/McDonald\'s-Ice-Cream-Machine-Hackers-Found-the-%E2%80%9CSmoking-Gun%E2%80%9D-That-Killed-Their-Startup-Security-GettyImages-sb10065848be-001.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
@@ -14,15 +23,6 @@ document.write(' \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Data Breach</span> <span class="mx-1">&bullet;</span> <span>December 14, 2023</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Ten Years Later, New Clues in the Target Breach</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/microsoft-digital-crime-unit-2023/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/657a0773d369f062c9dc83d8/master/pass/WorkingintheForensic_Print-1024x682-1024x682.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Vulnerability</span> <span class="mx-1">&bullet;</span> <span>December 14, 2023</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Microsoft’s Digital Crime Unit Goes Deep on How It Disrupts Cybercrime</h5> \
 </div> \
 </a> \
 </div> \
