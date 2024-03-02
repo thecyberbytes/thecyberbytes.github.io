4d4ccd303c2af745120afd588e230d664393b69a
GitHub Action <mailcyberbytes@gmail.com>
2024-03-02 03:07:04 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_13.js
+++ cyber_web/js/gen_card_col_13.js
@@ -13,11 +13,11 @@ document.write(' \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/section-702-privacy-reforms-sabotage-campaign/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65cfa7f5d4ec0093a1b6073d/master/pass/HOUSE-INTEL-DISINFO-CAMPAIGN-GettyImages-2013631709.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/spacex-hypersonic-missile-satellites/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65cecbd71c398a03e9494040/master/pass/SpaceX-launches-military-satellites-tuned-to-track-hypersonic-missiles-GettyImages-1935921645.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Hacking</span> <span class="mx-1">&bullet;</span> <span>February 16, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Leak of Russian ‘Threat’ Part of a Bid to Kill US Surveillance Reform, Sources Say</h2> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 16, 2024</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">SpaceX Launched Military Satellites Designed to Track Hypersonic Missiles</h2> \
 </div> \
 </a> \
 </div> \
