83e0bd988868baaba555d7b0d998d0128e1b39a4
GitHub Action <mailcyberbytes@gmail.com>
2024-03-03 19:03:42 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_15.js
+++ cyber_web/js/gen_card_col_15.js
@@ -13,11 +13,11 @@ document.write(' \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/stopped-using-passwords-passkeys/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65c410280986bc7aa98652f1/master/pass/security_passkey_password_login_future.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/2054-part-iv-a-nation-divided/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65c3d0a2fb69053cf3071cd6/master/pass/2054_Chapter_Four_Owen_Freeman.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 08, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">I Stopped Using Passwords. It\'s Great—and a Total Mess</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">2054, Part IV: A Nation Divided</h2> \
 </div> \
 </a> \
 </div> \
