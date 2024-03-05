4057e0e0ad9a80e84c7a0c4157921e81578a1a18
GitHub Action <mailcyberbytes@gmail.com>
2024-03-05 19:04:36 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_34.js
+++ cyber_web/js/gen_card_col_34.js
@@ -1,10 +1,10 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/cybersecurity-marginalized-communities-problem/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65c6b02bc303c7a2320b70b2/master/pass/020924-security-cybersecurity-civil-rights.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/2054-part-vi-standoff-at-arlington/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65c65eaab94a96bfb8315dd6/master/pass/2054_Chapter_Six_Owen_Freeman.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 12, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The Hidden Injustice of Cyberattacks</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">2054, Part VI: Standoff at Arlington</h5> \
 </div> \
 </a> \
 </div> \
