a006d6e20e548cb7797d0b989e836efbb2c6faca
GitHub Action <mailcyberbytes@gmail.com>
2024-03-16 03:08:02 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_32.js
+++ cyber_web/js/gen_card_col_32.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/china-cars-national-security-threat-investigation/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65dfc6054f673424018eca12/master/pass/Biden-Chinese-Vehicles-Security-1933433961.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 29, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The White House Warns Cars Made in China Could Unleash Chaos on US Highways</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/lockbit-fulton-county-georgia-trump-ransomware-leak/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/65e0b253054e55cd02eaf84d/master/pass/Lockbit-Ransomware-Gang-Leaks-Trump-Trial-Docs-From-Georgia-Court-Security-GettyImages-1601257378.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Ransomware</span> <span class="mx-1">&bullet;</span> <span>February 29, 2024</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">The Mysterious Case of the Missing Trump Trial Ransomware Leak</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/china-cars-national-security-threat-investigation/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65dfc6054f673424018eca12/master/pass/Biden-Chinese-Vehicles-Security-1933433961.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 29, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The White House Warns Cars Made in China Could Unleash Chaos on US Highways</h5> \
 </div> \
 </a> \
 </div> \
