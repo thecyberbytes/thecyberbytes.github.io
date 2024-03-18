0b856a7fcd98f435105f9c27ab57cde92da6c56c
GitHub Action <mailcyberbytes@gmail.com>
2024-03-18 03:08:19 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_22.js
+++ cyber_web/js/gen_card_col_22.js
@@ -1,19 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/here-come-the-ai-worms/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65e0e13fef1050f96f06c160/master/pass/security_ai_worm_email_spam.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/gps-ankle-tags-uk-privacy-illegal/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65e0d46dd4dc233b8a71f4d6/master/pass/Migrant-Surveillance-Tech-Suffers-Setback-with-Privacy-Ruling-Security-GettyImages-1271098982.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 01, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Here Come the AI Worms</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">The UK’s GPS Tagging of Migrants Has Been Ruled Illegal</h5> \
 </div> \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/china-cars-national-security-threat-investigation/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65dfc6054f673424018eca12/master/pass/Biden-Chinese-Vehicles-Security-1933433961.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/here-are-the-microsoft-and-google-security-updates-you-need-right-now/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65dfcc595e7030af474191d9/master/pass/022824-security-critical-updates-feb-01.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 29, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The White House Warns Cars Made in China Could Unleash Chaos on US Highways</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Here Are the Google and Microsoft Security Updates You Need Right Now</h5> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_34.js
+++ cyber_web/js/gen_card_col_34.js
@@ -18,11 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/apple-pq3-post-quantum-encryption/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65d60252f14794bc53bece50/master/pass/Apple%E2%80%99s-iMessage-Is-Getting-Post-Quantum-Encryption--Security-GettyImages-1978192381.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/anne-neuberger-cybersecurity-q-and-a/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65d50e8b735a52f796c0aa54/master/pass/Anne-Neuberger-Q&A-Security-GettyImages-1231220797.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 21, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Apple iOS 17.4: iMessage Gets Post-Quantum Encryption in New Update</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Anne Neuberger, a Top White House Cyber Official, Sees the \'Promise and Peril\' in AI</h5> \
 </div> \
 </a> \
 </div> \
