9617adfeb19954d7fe549107f07d5f7e9793c7ec
GitHub Action <mailcyberbytes@gmail.com>
2024-03-27 03:07:51 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_34.js
+++ cyber_web/js/gen_card_col_34.js
@@ -18,11 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/gps-tags-migrants/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65d90e463821b22f42c20077/master/pass/Britain-Is-GPS-Tagging-Thousands-of-Migrants-Security-GettyImages-1392356345.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/how-pentagon-learned-targeted-ads-to-find-targets-and-vladimir-putin/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65dcd3aeca2194c4e2e5c3e8/master/pass/meansofcontrol-final.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 27, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The UK Is GPS-Tagging Thousands of Migrants</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">How the Pentagon Learned to Use Targeted Ads to Find Its Targets—and Vladimir Putin</h5> \
 </div> \
 </a> \
 </div> \
