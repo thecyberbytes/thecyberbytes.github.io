70140917baa0e1b750db619cc95426458cc48c48
GitHub Action <mailcyberbytes@gmail.com>
2024-04-01 03:07:58 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_22.js
+++ cyber_web/js/gen_card_col_22.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/binance-top-investigator-detained-nigeria/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65ef6c0641cbe13ff11ffb94/master/pass/security_binance_executives_detained_nigeria.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 12, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Binance’s Top Crypto Crime Investigator Is Being Detained in Nigeria</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/hpsci-us-protests-section-702-presentation/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/65ef9003161cdb99119aefcc/master/pass/031124-security-section-702-privacy-reforms.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 12, 2024</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">US Lawmaker Cited NYC Protests in a Defense of Warrantless Spying</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/binance-top-investigator-detained-nigeria/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65ef6c0641cbe13ff11ffb94/master/pass/security_binance_executives_detained_nigeria.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 12, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Binance’s Top Crypto Crime Investigator Is Being Detained in Nigeria</h5> \
 </div> \
 </a> \
 </div> \
