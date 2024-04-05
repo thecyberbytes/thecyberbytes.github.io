5242321bd883b84a9a51dbc8f817cdfc702fff9a
GitHub Action <mailcyberbytes@gmail.com>
2024-04-05 19:07:35 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_25.js
+++ cyber_web/js/gen_card_col_25.js
@@ -1,14 +1,5 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/gps-ankle-tags-uk-privacy-illegal/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65e0d46dd4dc233b8a71f4d6/master/pass/Migrant-Surveillance-Tech-Suffers-Setback-with-Privacy-Ruling-Security-GettyImages-1271098982.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 01, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The UK’s GPS Tagging of Migrants Has Been Ruled Illegal</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/here-come-the-ai-worms/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/65e0e13fef1050f96f06c160/master/pass/security_ai_worm_email_spam.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
@@ -18,11 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://krebsonsecurity.com/2024/02/fulton-county-security-experts-call-lockbits-bluff/" target="_blank" style="text-decoration:none"> \
-<img src="https://krebsonsecurity.com/wp-content/uploads/2024/02/fultonga-lockbit.png" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/gps-ankle-tags-uk-privacy-illegal/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65e0d46dd4dc233b8a71f4d6/master/pass/Migrant-Surveillance-Tech-Suffers-Setback-with-Privacy-Ruling-Security-GettyImages-1271098982.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 01, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The UK’s GPS Tagging of Migrants Has Been Ruled Illegal</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/russia-ukraine-power-war-crimes/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65df8a8a4f673424018eca0d/master/pass/Satellite-Images-Reveal-Russia\'s-Attacks-on-Ukraine\'s-Power-Infrastructure-Security-GettyImages-1822098879.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>February 29, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Fulton County, Security Experts Call LockBit’s Bluff</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Russia Attacked Ukraine\'s Power Grid at Least 66 Times to ‘Freeze It Into Submission’</h5> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_34.js
+++ cyber_web/js/gen_card_col_34.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/registered-agents-inc-fake-personas/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/65de62d10e4d62d76c2529a7/master/pass/022724-security-registered-agents-inc.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 05, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Inside Registered Agents Inc., the Shadowy Firm Pushing the Limits of Business Privacy</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/facebook-two-factor-authentication-2fa-change/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/655564b0f213642b3b254446/master/pass/How-to-Opt-Out-of-Facebook%E2%80%99s-Latest-Two-Factor-Authentication-Change-Security-GettyImages-934277298.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 05, 2024</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">How to Turn Off Facebook’s Two-Factor Authentication Change</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/registered-agents-inc-fake-personas/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/65de62d10e4d62d76c2529a7/master/pass/022724-security-registered-agents-inc.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>March 05, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Inside Registered Agents Inc., the Shadowy Firm Pushing the Limits of Business Privacy</h5> \
 </div> \
 </a> \
 </div> \
