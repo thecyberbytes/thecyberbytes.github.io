149f72e71693d36e07ec04e256854f3b41ee2885
GitHub Action <mailcyberbytes@gmail.com>
2024-04-14 03:48:28 +0000

    Commiting Category data

--- cyber_web/js/gen_card_carousel.js
+++ cyber_web/js/gen_card_carousel.js
@@ -1,10 +1,10 @@
 document.write(' \
 <div class="carousel-item active"> \
-<a href="https://www.wired.com/story/roku-breach-hits-567000-users/" target="_blank"> \
-<img src="https://media.wired.com/photos/66199a7b3043e26dbcbb751d/master/pass/Roku-Data-Breach-GettyImages-1531859615.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<a href="https://www.wired.com/story/iran-israel-drone-attack-iron-dome/" target="_blank"> \
+<img src="https://media.wired.com/photos/661b18156f4d682d170b0481/master/pass/1242344494" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
 <div class="carousel-caption"> \
-<h2>Roku Breach Hits 567,000 Users</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 13, 2024</p> \
+<h2>How Israel Is Defending Against Iran\'s Drone Attack</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 14, 2024</p> \
 </div> \
 </a> \
 </div> \
@@ -18,11 +18,11 @@ document.write(' \
 </a> \
 </div> \
 <div class="carousel-item "> \
-<a href="https://www.wired.com/story/change-healthcare-ransomhub-threat/" target="_blank"> \
-<img src="https://media.wired.com/photos/66194e33b6dbceed427548e2/master/pass/RansomHub-Change-Healthcare-Alamy-W5BW53.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<a href="https://www.wired.com/story/roku-breach-hits-567000-users/" target="_blank"> \
+<img src="https://media.wired.com/photos/66199a7b3043e26dbcbb751d/master/pass/Roku-Data-Breach-GettyImages-1531859615.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
 <div class="carousel-caption"> \
-<h2>Change Healthcare Faces Another Ransomware Threat—and It Looks Credible</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 12, 2024</p> \
+<h2>Roku Breach Hits 567,000 Users</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 13, 2024</p> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_trending.js
+++ cyber_web/js/gen_card_trending.js
@@ -21,9 +21,9 @@ document.write(' \
 </a> \
 </li> \
 <li class="list-group-item" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/p4x-north-korea-internet-hacker-identity-reveal/" target="_blank" style="text-decoration:none"> \
+<a href="https://www.wired.com/story/iran-israel-drone-attack-iron-dome/" target="_blank" style="text-decoration:none"> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:10px"> \
-<h6 class="card-link text-dark font-weight-bold my-3">A Vigilante Hacker Took Down North Korea’s Internet. Now He’s Taking Off His Mask</h6> \
+<h6 class="card-link text-dark font-weight-bold my-3">How Israel Is Defending Against Iran\'s Drone Attack</h6> \
 </div> \
 </a> \
 </li> \
