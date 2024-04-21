6c594e825cb4e31e213ee484c0f0b3755c321bfb
GitHub Action <mailcyberbytes@gmail.com>
2024-04-21 03:07:37 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_24.js
+++ cyber_web/js/gen_card_col_24.js
@@ -4,7 +4,7 @@ document.write(' \
 <img src="https://media.wired.com/photos/65fdaf64b0027f5ffe444015/master/pass/security_ship_missile_internet_cables.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 01, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">A Ghost Ship’s Doomed Journey Through the Gate of Tears</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">How a Houthi-Bombed Ghost Ship Likely Cut Off Internet for Millions</h5> \
 </div> \
 </a> \
 </div> \
