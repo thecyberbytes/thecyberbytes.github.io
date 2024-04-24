544afb5fddd86217833167388944eb62dbfc3862
GitHub Action <mailcyberbytes@gmail.com>
2024-04-24 19:04:44 +0000

    Commiting Category data

--- cyber_web/js/gen_card_carousel.js
+++ cyber_web/js/gen_card_carousel.js
@@ -1,37 +1,37 @@
 document.write(' \
 <div class="carousel-item active"> \
+<a href="https://www.wired.com/story/arcanedoor-cyberspies-hacked-cisco-firewalls-to-access-government-networks/" target="_blank"> \
+<img src="https://media.wired.com/photos/66284838054b26b13d95a20c/master/pass/Cisco-Hack-Security-GettyImages-1238829383.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<div class="carousel-caption"> \
+<h2>\'ArcaneDoor\' Cyberspies Hacked Cisco Firewalls to Access Government Networks</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 24, 2024</p> \
+</div> \
+</a> \
+</div> \
+<div class="carousel-item "> \
+<a href="https://www.wired.com/story/best-vpn/" target="_blank"> \
+<img src="https://media.wired.com/photos/63eaa0cf1b87b32b35988c1e/master/pass/Best-VPNs-Guide-2023-Gear-GettyImages-1223865512.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<div class="carousel-caption"> \
+<h2>5 Best VPN Services (2024): For Routers, PC, iPhone, Android, and More</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 24, 2024</p> \
+</div> \
+</a> \
+</div> \
+<div class="carousel-item "> \
+<a href="https://www.wired.com/story/shotspotter-keeps-listening-contracts-expire/" target="_blank"> \
+<img src="https://media.wired.com/photos/662821c50ea1ff125a0f5cf2/master/pass/security_shotspotter_gunshot_data_police_contracts.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<div class="carousel-caption"> \
+<h2>ShotSpotter Keeps Listening for Gunfire After Contracts Expire</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 24, 2024</p> \
+</div> \
+</a> \
+</div> \
+<div class="carousel-item "> \
 <a href="https://www.wired.com/story/change-healthcare-admits-it-paid-ransomware-hackers/" target="_blank"> \
 <img src="https://media.wired.com/photos/66272ff030e8549d0b61fd5f/master/pass/516917488" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
 <div class="carousel-caption"> \
 <h2>Change Healthcare Finally Admits It Paid Ransomware Hackers—and Still Faces a Patient Data Leak</h2> \
 <p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 23, 2024</p> \
-</div> \
-</a> \
-</div> \
-<div class="carousel-item "> \
-<a href="https://www.wired.com/story/section-702-reauthorization-expansion/" target="_blank"> \
-<img src="https://media.wired.com/photos/66266f47405b8e297543db59/master/pass/702-wrapup-Security-GettyImages-2013631178.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
-<div class="carousel-caption"> \
-<h2>The Next US President Will Have Troubling New Surveillance Powers</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 22, 2024</p> \
-</div> \
-</a> \
-</div> \
-<div class="carousel-item "> \
-<a href="https://krebsonsecurity.com/2024/04/russian-fsb-counterintelligence-chief-gets-9-years-in-cybercrime-bribery-scheme/" target="_blank"> \
-<img src="https://krebsonsecurity.com/wp-content/uploads/2017/05/trumpsdumps-580x395.png" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
-<div class="carousel-caption"> \
-<h2>Russian FSB Counterintelligence Chief Gets 9 Years in Cybercrime Bribery Scheme</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 22, 2024</p> \
-</div> \
-</a> \
-</div> \
-<div class="carousel-item "> \
-<a href="https://www.wired.com/story/north-korea-amazon-max-animation-exposed-server/" target="_blank"> \
-<img src="https://media.wired.com/photos/6622f344e93e00504c86b731/master/pass/security_north_korea_tv_animation.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
-<div class="carousel-caption"> \
-<h2>North Koreans Secretly Animated Amazon and Max Shows, Researchers Say</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">April 22, 2024</p> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_30.js
+++ cyber_web/js/gen_card_col_30.js
@@ -1,10 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/iti-section-702-expansion-opposition/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/661fef9345f4d7f05434b8df/master/pass/GettyImages-2147862639%20(1).jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/yahoo-boys-real-time-deepfake-scams/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/661f0e85a5e870d3fe04b81a/master/pass/security_scammers_deepfake_fraud_nigeria.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 17, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Big Tech Says Spy Bill Turns Its Workers Into Informants</h5> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 18, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Real-Time Deepfake Romance Scams Have Arrived</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/the-trump-jury-has-a-doxing-problem/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/66212a8d666fc621c4ad55b6/master/pass/GettyImages-2147900581.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 18, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Trump Jury Has a Doxing Problem</h5> \
 </div> \
 </a> \
 </div> \
@@ -14,15 +23,6 @@ document.write(' \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 17, 2024</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Hackers Linked to Russia’s Military Claim Credit for Sabotaging US Water Utilities</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/senate-section-702-ecsr-provision/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/661dccae18a8cba39b4ef4c5/master/pass/Wiretapping-Bill-Senate-Security-1174425770.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 16, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">US Senate to Vote on a Wiretap Bill That Critics Call ‘Stasi-Like’</h5> \
 </div> \
 </a> \
 </div> \
