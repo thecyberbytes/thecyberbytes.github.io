a0514cbedfe9ec846e4356e79a6c6a3f5e08a6f4
GitHub Action <mailcyberbytes@gmail.com>
2024-05-03 03:08:49 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -1,19 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/identity-thief-lived-as-a-different-man-for-33-years/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/66104192c874bfc82b991645/master/pass/Fake-Identity-Security-Roundup-GettyImages-1718688461.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Crypto Currency</span> <span class="mx-1">&bullet;</span> <span>April 06, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">Identity Thief Lived as a Different Man for 33 Years</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/privacy-browsers-duckduckgo-ghostery-brave/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/5d02e96a5b82e3e7280511b7/master/pass/Security%20-%20Feature%20art%20-%20privacy%20browsers.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 06, 2024</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">Best Privacy Browsers (2024): Brave, Safari, Ghostery, Firefox, DuckDuckGo</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/identity-thief-lived-as-a-different-man-for-33-years/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/66104192c874bfc82b991645/master/pass/Fake-Identity-Security-Roundup-GettyImages-1718688461.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Crypto Currency</span> <span class="mx-1">&bullet;</span> <span>April 06, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">Identity Thief Lived as a Different Man for 33 Years</h5> \
 </div> \
 </a> \
 </div> \
