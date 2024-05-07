ea73e14d8c13b103d8f761b86dd9b0845f782632
GitHub Action <mailcyberbytes@gmail.com>
2024-05-07 03:09:05 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_13.js
+++ cyber_web/js/gen_card_col_13.js
@@ -6,7 +6,7 @@ document.write(' \
 <img src="https://media.wired.com/photos/661fef9345f4d7f05434b8df/master/pass/GettyImages-2147862639%20(1).jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 17, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Big Tech Squares Off Against US Spies</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">Big Tech Says Spy Bill Turns Its Workers Into Informants</h2> \
 </div> \
 </a> \
 </div> \
@@ -17,7 +17,7 @@ document.write(' \
 <img src="https://media.wired.com/photos/661fef9345f4d7f05434b8df/master/pass/GettyImages-2147862639%20(1).jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 17, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Big Tech Says Spy Bill Turns Its Workers Into Informants</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">Big Tech Squares Off Against US Spies</h2> \
 </div> \
 </a> \
 </div> \
