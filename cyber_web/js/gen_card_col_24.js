5c4d3c9b4366c5fb889d5d2c1f5913616fdc37cb
GitHub Action <mailcyberbytes@gmail.com>
2024-05-16 19:04:19 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_24.js
+++ cyber_web/js/gen_card_col_24.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/the-trump-jury-has-a-doxing-problem/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/66212a8d666fc621c4ad55b6/master/pass/GettyImages-2147900581.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 18, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The Trump Jury Has a Doxing Problem</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/yahoo-boys-real-time-deepfake-scams/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/661f0e85a5e870d3fe04b81a/master/pass/security_scammers_deepfake_fraud_nigeria.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 18, 2024</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">The Real-Time Deepfake Romance Scams Have Arrived</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/the-trump-jury-has-a-doxing-problem/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/66212a8d666fc621c4ad55b6/master/pass/GettyImages-2147900581.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 18, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The Trump Jury Has a Doxing Problem</h5> \
 </div> \
 </a> \
 </div> \
