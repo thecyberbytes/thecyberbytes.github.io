98c86373e21fcf8d9c76f068ee12af3196c2ce80
GitHub Action <mailcyberbytes@gmail.com>
2024-05-19 03:08:44 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_23.js
+++ cyber_web/js/gen_card_col_23.js
@@ -1,19 +1,19 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/racist-deepfake-google-cookies-gm-driver-surveillance-security-roundup/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/662bc744f4a60e1ae6c566db/master/pass/Baltimore-School-Impersonator-Security-GettyImages-1352083046.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/russia-un-resolution-space-nuclear-weapons-satellites/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/662bff9c7e1ff0154e168b16/master/pass/ars%20pickup_GettyImages-2147815932.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 27, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">School Employee Allegedly Framed a Principal With Racist Deepfake Rant</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">Russia Vetoed a UN Resolution to Ban Space Nukes</h5> \
 </div> \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/shotspotter-keeps-listening-contracts-expire/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/662821c50ea1ff125a0f5cf2/master/pass/security_shotspotter_gunshot_data_police_contracts.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/best-vpn/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/63eaa0cf1b87b32b35988c1e/master/pass/Best-VPNs-Guide-2023-Gear-GettyImages-1223865512.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 24, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">ShotSpotter Keeps Listening for Gunfire After Contracts Expire</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">5 Best VPN Services (2024): For Routers, PC, iPhone, Android, and More</h5> \
 </div> \
 </a> \
 </div> \
