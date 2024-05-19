7a06fc8756b25a8d03fc1be2031eecb796d86674
GitHub Action <mailcyberbytes@gmail.com>
2024-05-19 19:03:55 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_32.js
+++ cyber_web/js/gen_card_col_32.js
@@ -1,10 +1,10 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/the-dangerous-rise-of-gps-attacks/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/66312e416f2738b671e7fa40/master/pass/gps.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/biden-national-security-memorandum-critical-infrastructure-threats/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/66300a489a6a9c151b06d909/master/pass/White-House-Critical-Infrastructure-Framework-Security-GettyImages-1242853665.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 30, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The Dangerous Rise of GPS Attacks</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">The White House Has a New Master Plan to Stop Worst-Case Scenarios</h5> \
 </div> \
 </a> \
 </div> \
--- cyber_web/js/gen_card_col_35.js
+++ cyber_web/js/gen_card_col_35.js
@@ -13,7 +13,7 @@ document.write(' \
 <img src="https://media.wired.com/photos/661b18156f4d682d170b0481/master/pass/1242344494" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Hacking</span> <span class="mx-1">&bullet;</span> <span>April 14, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">How Israel Is Defending Against Iran\'s Drone Attack</h5> \
+<h5 class="card-link text-dark font-weight-bold my-3">How Israel Defended Against Iran\'s Drone and Missile Attack</h5> \
 </div> \
 </a> \
 </div> \
