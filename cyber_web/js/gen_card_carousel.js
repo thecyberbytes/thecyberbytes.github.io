ff7e92ad545a9b7d823ddcfbcef668805776c8a8
GitHub Action <mailcyberbytes@gmail.com>
2024-05-22 19:05:07 +0000

    Commiting Category data

--- cyber_web/js/gen_card_carousel.js
+++ cyber_web/js/gen_card_carousel.js
@@ -1,5 +1,14 @@
 document.write(' \
 <div class="carousel-item active"> \
+<a href="https://www.wired.com/story/tesla-ultra-wideband-radio-relay-attacks/" target="_blank"> \
+<img src="https://media.wired.com/photos/664e066fac6f505c466535a7/master/pass/Tesla-Relay-Attack-GettyImages-1282335939.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
+<div class="carousel-caption"> \
+<h2>Teslas Can Still Be Stolen With a Cheap Radio Hack—Despite New Keyless Tech</h2> \
+<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">May 22, 2024</p> \
+</div> \
+</a> \
+</div> \
+<div class="carousel-item "> \
 <a href="https://www.wired.com/story/sex-drugs-and-eventbrite/" target="_blank"> \
 <img src="https://media.wired.com/photos/664bce302299157f6700c89b/master/pass/security_eventbrite_scams_drugs_escorts.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
 <div class="carousel-caption"> \
@@ -22,15 +31,6 @@ document.write(' \
 <img src="https://media.wired.com/photos/644c45ad40f1b0ff57844439/master/pass/How-to-Remove-Your-Personal-Info-From-Google%E2%80%99s-Search-Results-Update-Gear-GettyImages-613331798.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
 <div class="carousel-caption"> \
 <h2>How to Remove Your Personal Info From Google’s Search Results</h2> \
-<p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">May 20, 2024</p> \
-</div> \
-</a> \
-</div> \
-<div class="carousel-item "> \
-<a href="https://www.wired.com/story/julian-assange-court-hearing-appeal-extradition/" target="_blank"> \
-<img src="https://media.wired.com/photos/664b0bdaec1993a6e92b82ae/master/pass/GettyImages-2107867029.jpg" alt="" class="d-block w-100" style="height:50vh;filter: brightness(50%);"> \
-<div class="carousel-caption"> \
-<h2>WikiLeaks’ Julian Assange Can Appeal His Extradition to the US, British Court Says</h2> \
 <p class="text-uppercase font-weight-bold" style="letter-spacing: 0.07rem;font-family:serif;font-size:12px">May 20, 2024</p> \
 </div> \
 </a> \
--- cyber_web/js/gen_card_col_11.js
+++ cyber_web/js/gen_card_col_11.js
@@ -2,22 +2,22 @@ document.write(' \
 <div class="container-fluid border border-dark border-0 p-0 m-0 align-items-center justify-content-between"> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/evolv-gun-detection-nyc-subways-emails/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/6629392ea620d99a9e8ea90e/master/pass/evolv-ny-subway-eric-adams.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://krebsonsecurity.com/2024/05/how-did-authorities-identify-the-alleged-lockbit-boss/" target="_blank" style="text-decoration:none"> \
+<img src="https://krebsonsecurity.com/wp-content/uploads/2024/05/khoroshev-sq.png" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>May 13, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Internal Emails Show How a Controversial Gun-Detection AI System Found Its Way to NYC</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">How Did Authorities Identify the Alleged Lockbit Boss?</h2> \
 </div> \
 </a> \
 </div> \
 </div> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/microsoft-gpt-4-us-spies-security-roundup/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/663e3bdc3aeee12d6ca9981b/master/pass/Security-Roundup-5-11-GettyImages-1484882668.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/evolv-gun-detection-nyc-subways-emails/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/6629392ea620d99a9e8ea90e/master/pass/evolv-ny-subway-eric-adams.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Vulnerability</span> <span class="mx-1">&bullet;</span> <span>May 11, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">Microsoft Deploys Generative AI for US Spies</h2> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>May 13, 2024</span></small> \
+<h2 class="card-link text-dark font-weight-bold my-3">Internal Emails Show How a Controversial Gun-Detection AI System Found Its Way to NYC</h2> \
 </div> \
 </a> \
 </div> \
