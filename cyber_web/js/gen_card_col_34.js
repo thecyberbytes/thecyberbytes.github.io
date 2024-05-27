bc71a81e024f806fa6cde89a4e257287d1ad4f5e
GitHub Action <mailcyberbytes@gmail.com>
2024-05-27 19:07:11 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_34.js
+++ cyber_web/js/gen_card_col_34.js
@@ -1,14 +1,5 @@
 document.write(' \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/arcanedoor-cyberspies-hacked-cisco-firewalls-to-access-government-networks/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/66284838054b26b13d95a20c/master/pass/Cisco-Hack-Security-GettyImages-1238829383.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 24, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">\'ArcaneDoor\' Cyberspies Hacked Cisco Firewalls to Access Government Networks</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/best-vpn/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/63eaa0cf1b87b32b35988c1e/master/pass/Best-VPNs-Guide-2023-Gear-GettyImages-1223865512.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
@@ -23,6 +14,15 @@ document.write(' \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 24, 2024</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">ShotSpotter Keeps Listening for Gunfire After Contracts Expire</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/arcanedoor-cyberspies-hacked-cisco-firewalls-to-access-government-networks/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/66284838054b26b13d95a20c/master/pass/Cisco-Hack-Security-GettyImages-1238829383.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>April 24, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">\'ArcaneDoor\' Cyberspies Hacked Cisco Firewalls to Access Government Networks</h5> \
 </div> \
 </a> \
 </div> \
