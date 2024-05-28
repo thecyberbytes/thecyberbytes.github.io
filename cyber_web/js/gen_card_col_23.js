6e94e14427a9f2f26225bbfa6799244fc6170357
GitHub Action <mailcyberbytes@gmail.com>
2024-05-28 03:08:19 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_23.js
+++ cyber_web/js/gen_card_col_23.js
@@ -9,20 +9,20 @@ document.write(' \
 </a> \
 </div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/cisa-cybersecurity-pledge/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/66316632d6935f24a3420a92/master/pass/CISA-pledge-Security-GettyImages-1322885895.jpg" class="card-img-top img-fluid" alt="..."> \
-<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
-<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>May 01, 2024</span></small> \
-<h5 class="card-link text-dark font-weight-bold my-3">The US Government Is Asking Big Tech to Promise Better Cybersecurity</h5> \
-</div> \
-</a> \
-</div> \
-<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
 <a href="https://www.wired.com/story/ai-crypto-tracing-model-money-laundering/" target="_blank" style="text-decoration:none"> \
 <img src="https://media.wired.com/photos/6631a1936dc0c77846852ed5/master/pass/Crypto-Money-Laundering-Security-GettyImages-1543076825.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>May 01, 2024</span></small> \
 <h5 class="card-link text-dark font-weight-bold my-3">A Vast New Data Set Could Supercharge the AI Hunt for Crypto Money Laundering</h5> \
+</div> \
+</a> \
+</div> \
+<div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
+<a href="https://www.wired.com/story/cisa-cybersecurity-pledge/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/66316632d6935f24a3420a92/master/pass/CISA-pledge-Security-GettyImages-1322885895.jpg" class="card-img-top img-fluid" alt="..."> \
+<div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;;font-size:12px"> \
+<small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>May 01, 2024</span></small> \
+<h5 class="card-link text-dark font-weight-bold my-3">The US Government Is Asking Big Tech to Promise Better Cybersecurity</h5> \
 </div> \
 </a> \
 </div> \
