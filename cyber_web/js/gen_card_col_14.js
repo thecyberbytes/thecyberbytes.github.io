0ec6086128154cdac7e39527e4010e9617af0acf
GitHub Action <mailcyberbytes@gmail.com>
2024-06-13 03:08:28 +0000

    Commiting Category data

--- cyber_web/js/gen_card_col_14.js
+++ cyber_web/js/gen_card_col_14.js
@@ -2,11 +2,11 @@ document.write(' \
 <div class="container-fluid border border-dark border-0 p-0 m-0 align-items-center justify-content-between"> \
 <div> \
 <div class="card mb-5 border-0 rounded zoom" style="background: #fafafa;"> \
-<a href="https://www.wired.com/story/remove-personal-info-from-google-search-results/" target="_blank" style="text-decoration:none"> \
-<img src="https://media.wired.com/photos/644c45ad40f1b0ff57844439/master/pass/How-to-Remove-Your-Personal-Info-From-Google%E2%80%99s-Search-Results-Update-Gear-GettyImages-613331798.jpg" class="card-img-top img-fluid" alt="..."> \
+<a href="https://www.wired.com/story/julian-assange-court-hearing-appeal-extradition/" target="_blank" style="text-decoration:none"> \
+<img src="https://media.wired.com/photos/664b0bdaec1993a6e92b82ae/master/pass/GettyImages-2107867029.jpg" class="card-img-top img-fluid" alt="..."> \
 <div class="card-body p-0 mx-0 my-3" style="letter-spacing: 0.07rem;font-family:serif;color:#ADADAD;font-size:12px"> \
 <small class="text-uppercase font-weight-bold"><span>Cyber Knowledge</span> <span class="mx-1">&bullet;</span> <span>May 20, 2024</span></small> \
-<h2 class="card-link text-dark font-weight-bold my-3">How to Remove Your Personal Info From Google’s Search Results</h2> \
+<h2 class="card-link text-dark font-weight-bold my-3">WikiLeaks’ Julian Assange Can Appeal His Extradition to the US, British Court Says</h2> \
 </div> \
 </a> \
 </div> \
